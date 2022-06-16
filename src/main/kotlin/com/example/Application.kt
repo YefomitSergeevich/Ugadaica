17de89bc0d27e5fe4b21ab0e577c734a3a2b2a6c
YefomitSergeevich <lihtimas@mail.ru>
2022-06-16 21:57:19 +0300

    Добавил вроде рабочий Swagger

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,12 +3,17 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import com.example.plugins.Swagger.configureSerialization
+import com.papsign.ktor.openapigen.OpenAPIGen
+import com.papsign.ktor.openapigen.schema.namer.DefaultSchemaNamer
+import com.papsign.ktor.openapigen.schema.namer.SchemaNamer
 import freemarker.cache.ClassTemplateLoader
 import freemarker.core.HTMLOutputFormat
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.freemarker.*
 import io.ktor.jackson.*
+import kotlin.reflect.KType
 
 fun main() {
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
@@ -18,6 +23,30 @@ fun main() {
         install(FreeMarker) {
             templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
             outputFormat = HTMLOutputFormat.INSTANCE
+        }
+        install(OpenAPIGen) {
+            // basic info
+            info {
+                version = "0.0.1"
+                title = "Test API"
+                description = "The Test API"
+                contact {
+                    name = "Support"
+                    email = "support@test.com"
+                }
+            }
+            // describe the server, add as many as you want
+            server("http://localhost:8080/") {
+                description = "Test server"
+            }
+            //optional custom schema object namer
+            replaceModule(DefaultSchemaNamer, object : SchemaNamer {
+                val regex = Regex("[A-Za-z0-9_.]+")
+                override fun get(type: KType): String {
+                    return type.toString().replace(regex) { it.value.split(".").last() }
+                        .replace(Regex(">|<|, "), "_")
+                }
+            })
         }
         registerMainRoutes()
         configureSerialization()
--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -1,18 +1,58 @@
 package com.example.plugins
 
-import io.ktor.jackson.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.features.*
+import com.papsign.ktor.openapigen.annotations.Response
+import com.papsign.ktor.openapigen.annotations.mapping.OpenAPIName
+import com.papsign.ktor.openapigen.annotations.parameters.HeaderParam
+import com.papsign.ktor.openapigen.annotations.parameters.PathParam
+import com.papsign.ktor.openapigen.annotations.parameters.QueryParam
+import com.papsign.ktor.openapigen.openAPIGen
+import com.papsign.ktor.openapigen.route.apiRouting
+import com.papsign.ktor.openapigen.route.info
+import com.papsign.ktor.openapigen.route.path.normal.get
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
-fun Application.configureSerialization() {
 
-    routing {
-        get("/json/jackson") {
-                call.respond(mapOf("hello" to "world"))
+object Swagger {
+    fun Application.configureSerialization() {
+        routing {
+            get("/openapi.json") {
+                call.respond(application.openAPIGen.api.serialize())
             }
+            get("/swagger-ui") {
+                call.respondRedirect("/swagger-ui/index.html?url=/openapi.json", true)
+            }
+        }
+        apiRouting {
+            route("header") {
+                get<NameParam, NameResponse>(
+                    info("Header Param Endpoint", "This is a Header Param Endpoint"),
+                    example = NameResponse("Mister")
+                ) { params ->
+                    respond(NameResponse("Hi, ${params.name}!"))
+                }
+            }
+            route("inline").get<StringParam, StringResponse>(
+                info("String Param Endpoint", "This is a String Param Endpoint"), // A Route module that describes an endpoint, it is optional
+                example = StringResponse("Hi")
+            ) { params ->
+                respond(StringResponse(params.a))
+            }
+        }
     }
+    data class NameParam(@HeaderParam("A simple Header Param") @OpenAPIName("X-NAME") val name: String)
+
+    data class NameResponse(@HeaderParam("A simple Header Param") @OpenAPIName("X-NAME") val name: String)
+
+    data class StringParam(
+        @PathParam("A simple String Param") val a: String,
+        @QueryParam("Optional String") val optional: String? // Nullable Types are optional
+    )
+
+    // A response can be any class, but a description will be generated from the annotation
+    @Response("A String Response")
+    data class StringResponse(val str: String)
 }
